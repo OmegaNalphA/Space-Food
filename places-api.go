390116b898080fcb194fe3a35502345d365a67a0
Anshul Aggarwal <OmegaNalphA@users.noreply.github.com>
2018-06-25 14:43:51 -0700

    updated, it works it just prints it out

--- places-api.go
+++ places-api.go
@@ -9,7 +9,16 @@ import (
 	"time"
 )
 
-var placesKey = "nah"
+var placesKey = "AIzaSyD8ebz6tM1xtHYHV6jgV-GakY_5oUthtRA"
+
+type latlon struct {
+	Latitude  string `json:"latitude"`
+	Longitude string `json:"longitude"`
+}
+
+type issPos struct {
+	Locate latlon `json:"iss_position"`
+}
 
 type placeid struct {
 	PlaceID string `json:"place_id"`
@@ -32,7 +41,9 @@ var pln = fmt.Println
 
 func main() {
 	// peopleInSpace()
-	places("Museum of Contemporary Art Australia")
+	// places("Museum of Contemporary Art Australia")
+	lat, lon := whereIsISS()
+	pln(findInRadius(lat, lon))
 }
 
 func places(inputText string) {
@@ -119,4 +130,84 @@ func peopleInSpace() {
 	for _, p := range people1.People {
 		pln(p.Name)
 	}
+}
+
+func whereIsISS() (string, string) {
+	url := "http://api.open-notify.org/iss-now.json"
+
+	spaceClient := http.Client{
+		Timeout: time.Second * 2,
+	}
+
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	req.Header.Set("User-Agent", "space-location-tutorial")
+
+	res, getErr := spaceClient.Do(req)
+	if getErr != nil {
+		log.Fatal(getErr)
+	}
+
+	if res.StatusCode == 404 {
+		log.Fatal("Hit a 404")
+	}
+
+	body, readErr := ioutil.ReadAll(res.Body)
+	if readErr != nil {
+		log.Fatal(readErr)
+	}
+
+	// pln(string(body))
+
+	location := issPos{}
+	jsonErr := json.Unmarshal(body, &location)
+	if jsonErr != nil {
+		log.Fatal(jsonErr)
+	}
+
+	return location.Locate.Latitude, location.Locate.Longitude
+}
+
+func findInRadius(lat string, lon string) string {
+	url := "https://maps.googleapis.com/maps/api/place/nearbysearch/json"
+
+	placesClient := http.Client{
+		Timeout: time.Second * 10,
+	}
+
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	req.Header.Set("User-Agent", "Testing how to query API's from parameters")
+	latt := lat + "," + lon
+	q := req.URL.Query()
+	q.Add("key", placesKey)
+	q.Add("location", latt)
+	q.Add("radius", "1500")
+	req.URL.RawQuery = q.Encode()
+	pln(req.URL.String())
+
+	res, getErr := placesClient.Do(req)
+	if getErr != nil {
+		log.Fatal(getErr)
+	}
+
+	// pln(res.StatusCode)
+	if res.StatusCode == 404 {
+		log.Fatal("Hit a 404")
+	}
+
+	body, readErr := ioutil.ReadAll(res.Body)
+	if readErr != nil {
+		log.Fatal(readErr)
+	}
+
+	// pln(string(body))
+
+	return string(body)
 }
